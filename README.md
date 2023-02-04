d295f5171f2045b20fa306c1b9288e1146b67edb
gillianbc <gszemeti@gmail.com>
2023-02-04 22:07:12 +0000

    Added README and requirements.txt for my venv

---
+++ README.md
@@ -0,0 +1,9 @@
+# Data Visualisation
+
+# Running from the Shell
+
+I have a venv containing matplotlib
+To run a python script outside of IntelliJ, I need to activate the venv before running the script e.g
+
+> source venv/bin/activate
+> python3 mpl_squares.py
--- mpl_squares.py
+++ mpl_squares.py
@@ -4,7 +4,15 @@ squares = [ 1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
 
+small_font = 14
+large_font = 24
+
 ax.plot(squares, linewidth=3)
 
+# Set chart title and label the axes
+ax.set_title("Square Numbers", fontsize=large_font)
+ax.set_xlabel("Value", fontsize=small_font)
+ax.set_ylabel("Square of Value", fontsize=small_font)
 
+# Show the plot
 plt.show()
